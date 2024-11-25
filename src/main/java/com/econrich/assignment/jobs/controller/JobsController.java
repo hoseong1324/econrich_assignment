ed4ec4d732063fabadc56b6dd709c6652175a93e
hoseong <hoseong1324@naver.com>
2024-11-25 12:19:32 +0900

    asciidoc modify & swagger description add

--- src/main/java/com/econrich/assignment/jobs/controller/JobsController.java
+++ src/main/java/com/econrich/assignment/jobs/controller/JobsController.java
@@ -5,6 +5,7 @@ import com.econrich.assignment.common.response.ResponseService;
 import com.econrich.assignment.jobs.dto.JobHistoryDto;
 import com.econrich.assignment.jobs.service.GetJobsService;
 import com.econrich.assignment.jobs.service.PatchJobsService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ public class JobsController {
 
 
     @GetMapping("/history/{employeeId}")
+    @Operation(summary = "해당 사원의 직업 내역을 조회합니다.",description = "직원 ID(employeeId)를 통해 해당 직원의 직업 내역을 조회힙니다.")
     public CommonResult getJobsHistory(@PathVariable int employeeId) {
         return responseService.getListResult(getJobsService.executeForHistory(employeeId));
     }
@@ -32,6 +34,7 @@ public class JobsController {
         return responseService.getSuccessResult();
     }
 
+    @Operation(summary = "특정 부서 %로 급여 인상", description = "최소급여 인상수치(minSalaryPercent) 또는 최대급여 인상수치(maxSalaryPercent) 를 통해 해당 부서의 급여를 인상합니다.")
     @PatchMapping(value = "/{jobId}/salary", consumes = MediaType.APPLICATION_JSON_VALUE)
     public CommonResult updateSalaryByPercent(@PathVariable String jobId, @RequestBody JobHistoryDto.UpdateSalaryByPercent form){
         patchJobsService.executeForPercentSalary(jobId, form);
