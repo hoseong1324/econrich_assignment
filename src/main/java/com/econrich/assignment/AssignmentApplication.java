a73798fc572cad342ac8d0ecb65775b729c2e2ce
hoseong <hoseong1324@naver.com>
2024-11-25 17:36:24 +0900

    boot timezone 설정

--- src/main/java/com/econrich/assignment/AssignmentApplication.java
+++ src/main/java/com/econrich/assignment/AssignmentApplication.java
@@ -1,12 +1,20 @@
 package com.econrich.assignment;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class AssignmentApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AssignmentApplication.class, args);
